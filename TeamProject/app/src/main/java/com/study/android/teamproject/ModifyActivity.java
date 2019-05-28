1829f19c05af333e7909ebdcb0e5158ff9e77fc9
jbisne <jisun7894@gmail.com>
2019-05-28 19:53:15 +0900

    2019-05-28 커밋

---
+++ TeamProject/app/src/main/java/com/study/android/teamproject/ModifyActivity.java
@@ -0,0 +1,15 @@
+package com.study.android.teamproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModifyActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify);
+    }
+}
--- TeamProject/app/src/main/java/com/study/android/teamproject/Mypage.java
+++ TeamProject/app/src/main/java/com/study/android/teamproject/Mypage.java
@@ -1,12 +1,14 @@
 package com.study.android.teamproject;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class Mypage extends Fragment
 {
@@ -19,6 +21,17 @@ public class Mypage extends Fragment
     {
         ViewGroup rootView =
                 (ViewGroup) inflater.inflate(R.layout.fragment_mypage, container, false);
+
+        Button button = rootView.findViewById(R.id.modifyButton);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(getActivity(),ModifyActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return rootView;
     }

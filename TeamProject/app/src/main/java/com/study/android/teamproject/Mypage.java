8434a4e88b29618d2a856f470c2d6a28683e9f37
jbisne <jisun7894@gmail.com>
2019-05-28 21:45:00 +0900

    2019-05-28 씨발얼럿창 못함 커밋

--- TeamProject/app/src/main/java/com/study/android/teamproject/Mypage.java
+++ TeamProject/app/src/main/java/com/study/android/teamproject/Mypage.java
@@ -1,14 +1,17 @@
 package com.study.android.teamproject;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Mypage extends Fragment
 {
@@ -22,8 +25,8 @@ public class Mypage extends Fragment
         ViewGroup rootView =
                 (ViewGroup) inflater.inflate(R.layout.fragment_mypage, container, false);
 
-        Button button = rootView.findViewById(R.id.modifyButton);
-        button.setOnClickListener(new View.OnClickListener()
+        Button button1 = rootView.findViewById(R.id.modifyButton);
+        button1.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
@@ -32,6 +35,33 @@ public class Mypage extends Fragment
                 startActivity(intent);
             }
         });
+
+
+        Button button2 = rootView.findViewById(R.id.logOutButton);
+        button2.setOnClickListener(new View.OnClickListener()
+        {
+
+        });
+
+        final String[] items = {"YES", "NO"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setTitle("title");
+        builder.setItems(items, new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(which ==0){
+                    /*..YES..*/
+                }
+                else{
+                    /*...NO...*/
+                }
+            }
+        });
+        builder.show();
+
 
         return rootView;
     }
